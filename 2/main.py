514c1282c8660fb226ad4d6774d08b7576b6e101
Inês <ines.c.paiva@tecnico.ulisboa.pt>
2023-10-07 02:33:20 +0100

    gaussian clausters

--- 2/main.py
+++ 2/main.py
@@ -6,20 +6,7 @@ from sklearn.metrics import mean_squared_error
 import numpy as np
 import itertools
 
-
- 
-## Load train data set
-def load_train_data(x_train_set_filename, y_train_set_filename): 
-    return np.load(x_train_set_filename), np.load(y_train_set_filename)
-
-def calculate_permutations(list_size, size_of_combination):
-    # Define size of the list for permutations   
-    elements = list(range(list_size))
-
-    # Generate all the permutations of 3 elements from the list and store them in a NumPy array
-    return np.array(list(itertools.combinations(elements, size_of_combination)))
-
-def models(x,y,lenght): 
+def models(x,y,lenght,alphas): 
     
     x_train_set= x
     y_train_set = y 
@@ -42,9 +29,9 @@ def models(x,y,lenght):
         x_test  = x_train_set_cpy[i : i+1]
         y_test = y_train_set_cpy[i : i+1] 
 
-        #model = Lasso(alpha=Alpha)
+        model = Lasso(alpha=alphas)
         #model = Ridge(alpha=alphas)
-        model = LinearRegression() 
+        #model = LinearRegression() 
         model.fit(x_train_set_cpy, y_train_set_cpy)
 
         
@@ -63,11 +50,6 @@ def models(x,y,lenght):
         Best_error = error
         return(Best_error)
         
-    
-    
-                
-
-
 
 
 def main():
@@ -75,22 +57,26 @@ def main():
     x = np.load("input_files/X_train_regression2.npy")
     y = np.load("input_files/y_train_regression2.npy")
 
-    # Combine X and y for each model
-    data = np.column_stack((x, y))
-    # Apply K-means clustering
+    model = LinearRegression() 
+    model.fit(x, y)
+
+        
+    # Test prediction with the part of the training set
+    y_pred = model.predict(x)
+
+    residuals = y - y_pred
+
+
     kmeans = KMeans(n_clusters=2, n_init=100, random_state=None)
-    kmeans.fit(data)
+    kmeans.fit(residuals.reshape(-1, 1))
+
+    cluster1_indices = np.where(kmeans.labels_ == 0)
+    cluster2_indices = np.where(kmeans.labels_ == 1)
 
     # Separate data points based on the cluster labels
-    model1_data = data[kmeans.labels_ == 0]
-    model2_data = data[kmeans.labels_ == 1]
+    cluster1_data = residuals[cluster1_indices]
+    cluster2_data = residuals[cluster2_indices]
 
-    # Extract X and y for each model
-    X1_train_set = model1_data[:, :-1]
-    Y1_train_set  = model1_data[:, -1]
-
-    X2_train_set  = model2_data[:, :-1]
-    Y2_train_set  = model2_data[:, -1]
 
     num_members_cluster1 = np.bincount(kmeans.labels_)[0]
     num_members_cluster2 = np.bincount(kmeans.labels_)[1]
@@ -98,15 +84,16 @@ def main():
     print(f"\nNumber of elements 1: {num_members_cluster1}\n")
     print(f"\nNumber of elements 2: {num_members_cluster2}\n")
 
-    #for Alpha in np.arange(0.01, 10, 0.01):
-    best_error_model1 = models(X1_train_set,Y1_train_set ,num_members_cluster1)
+    
+    Alpha=0.0001
+    best_error_model1 = models(x[cluster1_indices],y[cluster1_indices] ,num_members_cluster1,Alpha)
         
-    best_error_model2 = models(X2_train_set,Y2_train_set, num_members_cluster2)
+    best_error_model2 = models(x[cluster2_indices],y[cluster2_indices], num_members_cluster2,Alpha)
         
         
     
 
-        #print(f" Alpha: {Alpha}\n")
+    print(f" Alpha: {Alpha}\n")
     print(f" Mean of Error 1: {best_error_model1}")
     print(f" Mean of Error 2: {best_error_model2}")
             
